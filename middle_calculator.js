f00a5d0e488c792773c58aa3c79d5684aebec0b4
Edward-Mercado <edwardmercado149@gmail.com>
2025-09-25 09:21:29 -0700

    yuh huh

--- middle_calculator.js
+++ middle_calculator.js
@@ -9,17 +9,22 @@ function middle(weight_1, weight_2, weight_3) {
         console.log(weight_3)
     }
 }
+    function compare(weight_1, weight_2) {
+        if (weight_1 > weight_2) {
+            return true
+        }
+        else if (weight_1 < weight_2) {
+            return false
+        }
+        else {
+            return "equal"
+        }
+    }
 
-function compare(weight_1, weight_2) {
-    if (weight_1 > weight_2) {
-        return true
-    }
-    else if (weight_1 < weight_2) {
-        return false
-    }
-    else {
-        return "equal"
-    }
+function short_middle(weight_1, weight_2, weight_3) {
+    const weights = [weight_1, weight_2, weight_3];
+    weights.sort();
+    console.log(weights[1])
 }
 
-middle(1045, 1040, 1090)
+short_middle(105, 100, 10)
