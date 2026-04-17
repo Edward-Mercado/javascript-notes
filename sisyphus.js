5d1fb55755020321087fd431b98756fb401e2db3
Edward-Mercado <edwardmercado149@gmail.com>
2026-04-17 12:14:37 -0400

    asdf

--- sisyphus.js
+++ sisyphus.js
@@ -1,3 +1,6 @@
+import { setTimeout } from 'timers/promises';
+
+
 let sisyphus = {
     mountain: ["boulder"]
 }
@@ -7,10 +10,15 @@ let index = 0
 while(index !== sisyphus.mountain.length) {
     sisyphus.mountain.push("boulder")
     index++
-    if(Math.random() > 0.99) {
+    let event = Math.random()
+    if(event > 0.95) {
         sisyphus.mountain.length = 1
         index = 0
+        console.log("The boulder rolled down the hill.")
+    } else if (event < 0.2) {
+        console.log("Pushing...")
     }
+    await setTimeout(2000)
 }
 
 console.log("The boulder has been rolled up the hill.")
