8b77367aa4b0a7b59425c6f638bd5b5ebffe9527
Edward-Mercado <edwardmercado149@gmail.com>
2026-07-04 01:58:16 -0400

    kn

--- apitest.ts
+++ apitest.ts
@@ -2,12 +2,14 @@ async function getAPIshit() {
     let quote = await fetch('https://thequoteshub.com/api/random-quote')
     .then(async (response) => {
         let quoteJSON = await response.json()
-        console.log(quoteJSON)
+        console.log(quoteJSON.text)
+        console.log(" ")
+        console.log(quoteJSON.author)
     })
 }
 
 async function getMoreAPIshit() {
-    let quote = await fetch('https://random-word-api.herokuapp.com/word?number=42&lang=de')
+    let quote = await fetch('https://random-word-api.herokuapp.com/word?number=42&lang=en')
     .then(async (response) => {
         let quoteJSON = await response.json()
         console.log(quoteJSON)
